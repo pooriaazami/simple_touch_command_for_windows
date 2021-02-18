e252ae25bacda70630c64a11cc78d8ae054feee4
Pooria Azami <pooriaazami@gmail.com>
2021-02-18 14:49:53 +0330

    code for creating file added

--- touch.cpp
+++ touch.cpp
@@ -1,23 +1,31 @@
 
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <fstream>
+#include <cstring>
 
-int main(int argc, char* argv[])
+inline void create_file(std::string);
+
+int main(int argc, char *argv[])
 {
-    if(argc < 2)
+    if (argc < 2)
         std::cerr << "Not enough input" << std::endl;
 
-    
     std::string base_path(argv[0]);
     base_path = base_path.substr(0, base_path.length() - 9);
 
-    for(int i = 1 ; i < argc ; i++)
+    for (int i = 1; i < argc; i++)
     {
         std::string file_path = (base_path + "\\").append(argv[i]);
 
-        std::cout << file_path << std::endl;
+        create_file(file_path);
     }
 
-
     return 0;
+}
+
+inline void create_file(std::string input)
+{
+    std::fstream file_stream;
+    file_stream.open(input.data(), std::ios::out);
+    file_stream.close();
 }
