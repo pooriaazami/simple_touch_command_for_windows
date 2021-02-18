2ff25f7447e3392c075ad8e48bc14a9ef29a83ff
Pooria Azami <pooriaazami@gmail.com>
2021-02-18 14:41:49 +0330

    input processor code added

--- touch.cpp
+++ touch.cpp
@@ -7,6 +7,17 @@ int main(int argc, char* argv[])
     if(argc < 2)
         std::cerr << "Not enough input" << std::endl;
 
+    
+    std::string base_path(argv[0]);
+    base_path = base_path.substr(0, base_path.length() - 9);
+
+    for(int i = 1 ; i < argc ; i++)
+    {
+        std::string file_path = (base_path + "\\").append(argv[i]);
+
+        std::cout << file_path << std::endl;
+    }
+
 
     return 0;
 }
